e364fc6a274fd43ba577304cb6f58d25a625c170
Florencekyarikunda <florencekyarikund@gmail.com>
2022-08-13 13:16:03 +0300

    form

---
+++ script.js
@@ -0,0 +1,73 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["firstName"] = document.getElementById("firstName").value;
+    formData["secondName"] = document.getElementById("secondName").value;
+    formData["thirdName"] = document.getElementById("thirdName").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.firstName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.secondName;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.thirdName;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("firstName").value = "";
+    document.getElementById("secondName").value = "";
+    document.getElementById("thirdName").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("firstName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("secondName").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("thirdName").value = selectedRow.cells[2].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.firstName;
+    selectedRow.cells[1].innerHTML = formData.secondName;
+    selectedRow.cells[2].innerHTML = formData.thirdName;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("firstName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
